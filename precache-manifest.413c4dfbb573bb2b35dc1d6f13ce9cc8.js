45c58aa9fd10037b1137802c503f4a391f548c23
Travis <travis@travis-ci.org>
2018-11-06 12:56:34 +0000

    Update to ffcce38863b03878e9f95f15739d30583860bbb4.

---
+++ precache-manifest.413c4dfbb573bb2b35dc1d6f13ce9cc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71b16b14d2d86a205408",
+    "url": "https://solid.github.io/ldflex-playground/static/js/runtime~main.71b16b14.js"
+  },
+  {
+    "revision": "9fd29d27bb6afb97ad68",
+    "url": "https://solid.github.io/ldflex-playground/static/js/main.9fd29d27.chunk.js"
+  },
+  {
+    "revision": "406a455168fb5bd933d9",
+    "url": "https://solid.github.io/ldflex-playground/static/js/1.406a4551.chunk.js"
+  },
+  {
+    "revision": "9fd29d27bb6afb97ad68",
+    "url": "https://solid.github.io/ldflex-playground/static/css/main.4cae822d.chunk.css"
+  },
+  {
+    "revision": "11c03229298db1f80dca18d81b014eb5",
+    "url": "https://solid.github.io/ldflex-playground/index.html"
+  }
+];
