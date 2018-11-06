54d37777bb8db9e05254905e8513b7d1e42fbddd
Ruben Verborgh <ruben@verborgh.org>
2018-11-06 12:23:39 +0100

    Add playground.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Playground from './Playground';
 import './App.css';
+
+const defaultExpression = "['https://ruben.verborgh.org/profile/#me'].friends.firstName";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header/>
-        <Footer/>
-      </div>
+    <div>
+      <Header/>
+      <main>
+        <h2>
+          Write your 
+          <a href="https://solid.github.io/query-ldflex/">
+            Solid LDflex</a> expression
+        </h2>
+        <Playground expression={defaultExpression}/>
+      </main>
+      <Footer/>
+    </div>
     );
   }
 }
---
+++ src/Playground.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Value, List } from '@solid/react';
+import './Playground.css';
+
+export default class Playground extends React.Component {
+  state = { expression: this.props.expression };
+
+  componentDidUpdate({ expression }) {
+    if (this.props.expression !== expression)
+      this.setState({ expression: this.props.expression });
+  }
+
+  render() {
+    const { expression } = this.state;
+    return (
+      <div className="playground">
+        <p className="expression">
+          <label><code>solid.data</code></label>
+          <input value={expression}
+                 onChange={e => this.setState({ expression: e.target.value })}/>
+        </p>
+        <h3>Single result</h3>
+        <p className="single"><Value src={`${expression}`}/></p>
+        <h3>Multiple results (first 10)</h3>
+        <List src={expression} limit="10"/>
+        <h3>Corresponding SPARQL query</h3>
+        <pre className="sparql"><code>
+          <Value src={`${expression}.sparql`}/>
+        </code></pre>
+      </div>
+    );
+  }
+}
