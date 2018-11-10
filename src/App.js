df0da2769e48f86e582701c9fb503ceb9fa8fd00
Ruben Verborgh <ruben@verborgh.org>
2018-11-10 20:14:56 +0100

    Use logged-in user's WebID as example.
    Closes https://github.com/solid/ldflex-playground/issues/3.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Playground from './Playground';
+import { withWebId } from '@solid/react';
 import './App.css';
 
 const defaultExpression = "['https://ruben.verborgh.org/profile/#me'].friends.firstName";
 
 class App extends React.Component {
+  state = this.savedState;
+
   // Save the state in the URL fragment
   get savedState() {
-    return { expression: window.location.hash.substr(1) };
+    return { expression: decodeURIComponent(window.location.hash.substr(1)) };
   }
   set savedState({ expression }) {
-    window.location.replace(`#${expression}`);
+    window.location.replace(`#${encodeURIComponent(expression)}`);
+  }
+
+  componentDidUpdate({ webId }, { expression }) {
+    // If the user just logged in, show an example with their WebID
+    if (!this.savedState.expression && this.props.webId !== webId)
+      this.setState({ expression: `[${this.props.webId}].name` });
+    // Save the expression if it changed
+    if (this.state.expression !== expression)
+      this.savedState = this.state;
   }
 
   render() {
@@ -25,12 +37,12 @@ class App extends React.Component {
           <a href="https://solid.github.io/query-ldflex/">
             Solid LDflex</a> expression
         </h2>
-        <Playground expression={this.savedState.expression || defaultExpression}
-                    onExpressionChange={e => this.savedState = { expression: e }}/>
+        <Playground expression={this.state.expression || defaultExpression }
+                    onExpressionChange={e => this.setState({ expression: e })}/>
       </main>
       <Footer/>
     </div>
     );
   }
 }
-export default App;
+export default withWebId(App);
