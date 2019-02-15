ef6c294d2c68033eafe54f0a3856703026492cc4
Travis <travis@travis-ci.org>
2019-02-15 15:39:59 +0000

    Update to 61d6e2b239b9204951ae4dfd50825cb8c9f2749e.

---
+++ precache-manifest.65f0440a93daf0e6ae5a978dc90d32b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a22f9cc3fbcfe485bee25a386006527e",
+    "url": "https://solid.github.io/ldflex-playground/static/media/solid.a22f9cc3.svg"
+  },
+  {
+    "revision": "b334fb83ef64914a1002",
+    "url": "https://solid.github.io/ldflex-playground/static/js/runtime~main.b334fb83.js"
+  },
+  {
+    "revision": "2c98c5fc980a730d9f2b",
+    "url": "https://solid.github.io/ldflex-playground/static/js/main.2c98c5fc.chunk.js"
+  },
+  {
+    "revision": "0e62b9ad7a39bbf18d22",
+    "url": "https://solid.github.io/ldflex-playground/static/js/2.0e62b9ad.chunk.js"
+  },
+  {
+    "revision": "2c98c5fc980a730d9f2b",
+    "url": "https://solid.github.io/ldflex-playground/static/css/main.915d9056.chunk.css"
+  },
+  {
+    "revision": "e89d2499b9785342142dd687d5b2de19",
+    "url": "https://solid.github.io/ldflex-playground/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://solid.github.io/ldflex-playground/precache-manifest.0831fd03da56aab2fe5061bf409a72e8.js"
+  "https://solid.github.io/ldflex-playground/precache-manifest.65f0440a93daf0e6ae5a978dc90d32b2.js"
 );
 
 workbox.clientsClaim();
