bc0d8cdc21338f257a3501d806630f213ff530e6
Ruben Verborgh <ruben@verborgh.org>
2019-03-20 16:04:22 -0400

    Toggle between single or multiple results.

--- src/Playground.js
+++ src/Playground.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Value, List } from '@solid/react';
-import { evaluate, isSafe } from './expressions';
 import './Playground.css';
 
 export default class Playground extends React.Component {
-  state = this.updateExpression();
+  state = {
+    max: 20,
+    multiple: true,
+    ...this.updateExpression(),
+  }
 
   componentDidUpdate({ expression }) {
     if (this.props.expression !== expression)
@@ -13,23 +16,28 @@ export default class Playground extends React.Component {
 
   updateExpression(input) {
     const expression = input || this.props.expression || '';
-    const safe = isSafe(expression);
+    // An expression has no side effects if it has no method calls
+    // (conservatively identified by a parenthesis)
+    const safe = !expression.includes('(');
     const state = {
       input: expression,
       // Don't evaluate unsafe expressions unless the user asks
       expression: input || safe ? expression : '',
-      evaluation: input || safe ? evaluate(expression) : '',
       // Avoid evaluating unsafe expressions a second time
       sparql: expression && safe ? `${expression}.sparql` : '',
     };
     return this.state ? this.setState(state) : state;
   }
 
-  onChangeInput = event => {
+  onChangeExpression = event => {
     this.setState({ input: event.target.value });
   }
 
-  onSubmitInput = event => {
+  onChangeMultiple = event => {
+    this.setState({ multiple: event.target.checked });
+  }
+
+  onExecute = event => {
     event.preventDefault();
     const { input } = this.state;
     this.updateExpression(input);
@@ -38,24 +46,34 @@ export default class Playground extends React.Component {
   }
 
   render() {
-    const { input, expression, evaluation, sparql } = this.state;
+    const { input, multiple, max, expression, sparql } = this.state;
     return (
-      <form className="playground" onSubmit={this.onSubmitInput}>
+      <form className="playground" onSubmit={this.onExecute}>
         <p className="expression">
           <label><code>solid.data</code></label>
-          <input value={input} onChange={this.onChangeInput} required />
+          <input value={input} onChange={this.onChangeExpression} required />
           <button>Execute</button>
         </p>
+        <p className="multiple">
+          <label>
+            <input type="checkbox" checked={multiple} onChange={this.onChangeMultiple}/>
+            multiple results
+          </label>
+        </p>
+
         <h3>Expression</h3>
         <pre className="expression"><code>{expression}</code></pre>
 
-        <h3>Single result</h3>
-        <p className="single"><Value src={evaluation}/></p>
-
-        <h3>Multiple results (first 10)</h3>
-        <List src={evaluation} limit="10">{(item, i) =>
-          <li key={i}>{linkTo(`${item}`)}</li>}
-        </List>
+        { !multiple ? <div>
+            <h3>Single result</h3>
+            <p className="single"><Value src={expression}/></p>
+          </div> : <div>
+            <h3>Multiple results <em>(first {max})</em></h3>
+            <List src={expression} limit={max}>{(item, i) =>
+              <li key={i}>{linkTo(`${item}`)}</li>}
+            </List>
+          </div>
+        }
 
         <h3>Corresponding SPARQL query</h3>
         <pre className="sparql"><code><Value src={sparql}/></code></pre>
